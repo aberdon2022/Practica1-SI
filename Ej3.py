1ceef01edaaa8d59d3a4f52ff9f73c8474d53744
aberdon2022 <alejandrob25082003@gmail.com>
2025-03-10 15:33:58 +0100

    Segundo apartado Ej3

--- Ej3.py
+++ Ej3.py
@@ -26,7 +26,8 @@ con.close()
 
 df = update_fecha_cierre(df)
 
-
+df['fecha_a'] = pd.to_datetime(df['fecha_a'])
+df['dia'] = df['fecha_a'].dt.day_name()
 
 df_por_empleado_fraude = df[df['tipo_incidencia'] == 'Fraude'].groupby('empleado').agg({'ticket_id': 'count'}).reset_index().rename(columns={'ticket_id': 'count'})
 
@@ -38,4 +39,14 @@ print(df_por_empleado_fraude.to_string())
 print(df_por_nivel_fraude.to_string())
 print(df_por_cliente_fraude.to_string())
 
+# Numero de actuaciones por empleado
 
+df_contactos_empleado = df[df['tipo_incidencia'] == 'Fraude'].groupby('empleado').agg({'ticket_id': 'count'}).reset_index().rename(columns={'ticket_id': 'count'})
+df_contactos_empleado_por_nivel = df[df['tipo_incidencia'] == 'Fraude'].groupby(['nivel_empleado']).agg({'ticket_id': 'count'}).reset_index().rename(columns={'ticket_id': 'count'})
+df_contactos_cliente = df[df['tipo_incidencia'] == 'Fraude'].groupby(['cliente']).agg({'ticket_id': 'count'}).reset_index().rename(columns={'ticket_id': 'count'})
+df_contactos_por_dia = df[df['tipo_incidencia'] == 'Fraude'].groupby(['dia']).agg({'ticket_id': 'count'}).reset_index().rename(columns={'ticket_id': 'count'})
+
+print(df_contactos_empleado.to_string())
+print(df_contactos_empleado_por_nivel.to_string())
+print(df_contactos_cliente.to_string())
+print(df_contactos_por_dia.to_string())
