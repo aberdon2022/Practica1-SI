e20346f0777b3342e03d1db1d64aa58e21b32181
vbv-18 <shafariens23@gmail.com>
2025-03-14 20:01:42 +0100

    Analisis estadistico arreglado

--- Ej3.py
+++ Ej3.py
@@ -59,13 +59,15 @@ print(df_contactos_por_dia.to_string())
 
 #Analisis estadistico
 
-estadisticas_empleado = df[df['tipo_incidencia'] == 'Fraude'].groupby('empleado').agg(fraude_count=('ticket_id', 'count')).agg(['median', 'mean', 'var', 'min', 'max']).reset_index()
-estadisticas_nivel = df[df['tipo_incidencia'] == 'Fraude'].groupby('nivel_empleado').agg(fraude_count=('ticket_id', 'count')).agg(['median', 'mean', 'var', 'min', 'max']).reset_index()
-estadisticas_cliente = df[df['tipo_incidencia'] == 'Fraude'].groupby('cliente').agg(fraude_count=('ticket_id', 'count')).agg(['median', 'mean', 'var', 'min', 'max']).reset_index()
-estadisticas_dia = df[df['tipo_incidencia'] == 'Fraude'].groupby('dia').agg(fraude_count=('ticket_id', 'count')).agg(['median', 'mean', 'var', 'min', 'max']).reset_index()
+estadisticas_empleado = df[df['tipo_incidencia'] == 'Fraude'].groupby('empleado').agg({'ticket_id':['count','median', 'mean', 'var', 'min', 'max']}).reset_index()
+estadisticas_nivel = df[df['tipo_incidencia'] == 'Fraude'].groupby('nivel_empleado').agg({'ticket_id':['count','median', 'mean', 'var', 'min', 'max']}).reset_index()
+estadisticas_cliente = df[df['tipo_incidencia'] == 'Fraude'].groupby('cliente').agg({'ticket_id':['count','median', 'mean', 'var', 'min', 'max']}).reset_index()
+estadisticas_tipo_incidencia = df.groupby('tipo_incidencia').agg({'ticket_id':['count','median', 'mean', 'var', 'min', 'max']}).reset_index()
+estadisticas_dia = df[df['tipo_incidencia'] == 'Fraude'].groupby('dia').agg({'ticket_id':['count','median', 'mean', 'var', 'min', 'max']}).reset_index()
 
 print()
 print("Estadisticas por empleado\n" + estadisticas_empleado.to_string() + "\n")
 print("Estadisticas por nivel de empleado\n" + estadisticas_nivel.to_string() + "\n")
 print("Estadisticas por cliente\n" + estadisticas_cliente.to_string() + "\n")
+print("Estadisticas por tipos de incidencia\n" + estadisticas_tipo_incidencia.to_string() + "\n")
 print("Estadisticas por dia\n" +estadisticas_dia.to_string() + "\n")
